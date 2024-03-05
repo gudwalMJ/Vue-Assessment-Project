f17e36c58526515d2d3e47ec9dcc5ca346da3e42
Josip Milan <josip.milan@aol.com>
2024-03-05 11:59:05 +0100

    additional styling

--- src/components/NewListingForm.vue
+++ src/components/NewListingForm.vue
@@ -4,7 +4,7 @@
       ><img :src="backIcon" alt="back" />Back to overview</a
     >
     <h1>Create New Listing</h1>
-    <form @submit.prevent="submitForm">
+    <form @submit.prevent="submitForm" class="form-layout">
       <!-- Input field for street name -->
       <div class="input-container">
         <label for="street-name">Street Name*</label>
@@ -17,25 +17,27 @@
         />
       </div>
       <!-- Input field for house number -->
-      <div class="input-container">
-        <label for="house-number">House Number*</label>
-        <input
-          id="house-number"
-          v-model.trim="listing.houseNumber"
-          type="number"
-          placeholder="Enter house number"
-          required
-        />
-      </div>
-      <!-- Input field for house number addition -->
-      <div class="input-container">
-        <label for="addition">Addition (optional)*</label>
-        <input
-          id="number-addition"
-          v-model.trim="listing.numberAddition"
-          type="text"
-          placeholder="e.g. A"
-        />
+      <div class="row">
+        <div class="input-container half">
+          <label for="house-number">House Number*</label>
+          <input
+            id="house-number"
+            v-model.trim="listing.houseNumber"
+            type="number"
+            placeholder="Enter house number"
+            required
+          />
+        </div>
+        <!-- Input field for house number addition -->
+        <div class="input-container half">
+          <label for="addition">Addition (optional)*</label>
+          <input
+            id="number-addition"
+            v-model.trim="listing.numberAddition"
+            type="text"
+            placeholder="e.g. A"
+          />
+        </div>
       </div>
       <!-- Input field for ZIP code -->
       <div class="input-container">
@@ -78,53 +80,49 @@
         />
       </div>
       <!-- Input field for size -->
-      <div class="input-container">
-        <label for="size">Size*</label>
-        <input
-          v-model.number="listing.size"
-          type="number"
-          placeholder="e.g. 60m2"
-          required
-          min="0"
-        />
-      </div>
-      <!-- Input field for garage -->
-      <div class="input-container">
-        <label for="garage-input">Garage*</label>
-        <input
-          list="garage-options"
-          id="garage-input"
-          v-model="listing.garageText"
-          @change="updateGarageValue"
-          placeholder="Select"
-          required
-        />
-        <datalist id="garage-options">
-          <option value="Yes"></option>
-          <option value="No"></option>
-        </datalist>
+      <div class="row">
+        <div class="input-container half">
+          <label for="size">Size*</label>
+          <input
+            v-model.number="listing.size"
+            type="number"
+            placeholder="e.g. 60m2"
+            required
+            min="0"
+          />
+        </div>
+        <!-- Input field for garage -->
+        <div class="input-container half">
+          <label for="garage-select">Garage*</label>
+          <select id="garage-select" v-model="listing.garage" required>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
       </div>
       <!-- Input field for bedrooms -->
-      <div class="input-container">
-        <label for="bedrooms">Bedrooms*</label>
-        <input
-          v-model.number="listing.bedrooms"
-          type="number"
-          placeholder="Enter amount"
-          required
-          min="0"
-        />
-      </div>
-      <!-- Input field for bathrooms -->
-      <div class="input-container">
-        <label for="bathrooms">Bathrooms*</label>
-        <input
-          v-model.number="listing.bathrooms"
-          type="number"
-          placeholder="Enter amount"
-          required
-          min="0"
-        />
+      <div class="row">
+        <div class="input-container half">
+          <label for="bedrooms">Bedrooms*</label>
+          <input
+            v-model.number="listing.bedrooms"
+            type="number"
+            placeholder="Enter amount"
+            required
+            min="0"
+          />
+        </div>
+        <!-- Input field for bathrooms -->
+        <div class="input-container half">
+          <label for="bathrooms">Bathrooms*</label>
+          <input
+            v-model.number="listing.bathrooms"
+            type="number"
+            placeholder="Enter amount"
+            required
+            min="0"
+          />
+        </div>
       </div>
       <!-- Input field for construction year -->
       <div class="input-container">
@@ -140,14 +138,22 @@
       <!-- Input field for description -->
       <div class="input-container">
         <label for="description">Description*</label>
-        <input
+        <textarea
+          id="description"
           v-model.trim="listing.description"
           placeholder="Enter description"
           required
-        />
+          rows="4"
+        ></textarea>
       </div>
       <!-- Submit button -->
-      <button type="submit">POST</button>
+      <button
+        type="submit"
+        :disabled="!formValid"
+        :class="{ 'button-active': formValid }"
+      >
+        POST
+      </button>
 
       <!-- Display error message -->
       <p v-if="error">{{ errorMessage }}</p>
@@ -167,12 +173,41 @@ export default {
       backIcon,
       listing: {
         streetName: "",
-        houseNumber: "",
-        // ...other properties...
+        houseNumber: null,
+        numberAddition: "",
+        zip: "",
+        city: "",
+        price: null,
+        size: null,
+        bedrooms: null,
+        bathrooms: null,
+        constructionYear: null,
+        garage: "",
+        description: "",
+        image: null,
       },
       error: false,
       errorMessage: "",
     };
+  },
+  computed: {
+    formValid() {
+      return (
+        this.listing.streetName.trim() !== "" &&
+        this.listing.houseNumber !== null && // Assuming houseNumber is initialized as null
+        this.listing.zip.trim() !== "" &&
+        this.listing.city.trim() !== "" &&
+        this.listing.price > 0 &&
+        this.listing.size > 0 &&
+        this.listing.bedrooms > 0 &&
+        this.listing.bathrooms > 0 &&
+        this.listing.constructionYear > 0 &&
+        this.listing.garage !== "" &&
+        this.listing.description.trim() !== "" &&
+        // Assuming image is required, ensure it has a value
+        this.listing.image
+      );
+    },
   },
   methods: {
     validateForm() {
@@ -268,6 +303,14 @@ export default {
   background-size: cover;
   background-position: center;
   padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.form-layout {
+  margin-left: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 .new-listing-page h1 {
   margin-top: 50px;
@@ -278,8 +321,45 @@ export default {
 }
 
 /* Input field styles */
-.new-listing-page .input-container {
+.input-container {
+  display: flex;
+  flex-direction: column;
   margin-bottom: 20px;
+  width: 420px;
+}
+.input-container label {
+  align-self: flex-start; /* Aligns the label to the left */
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+.input-container.half {
+  flex: 1;
+  flex-grow: 0;
+  flex-shrink: 0;
+}
+.input-container.half:first-child {
+  flex-basis: 200px;
+}
+.input-container.half:last-child {
+  flex-grow: 1;
+  margin-left: 10px;
+}
+.row {
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+}
+.row .input-container {
+  width: 200px; /* Smaller width for input containers inside a row */
+}
+.new-listing-page textarea {
+  padding: 10px;
+  border: 1px;
+  border-radius: 5px;
+  width: 100%;
+  height: auto;
+  box-sizing: border-box;
+  resize: none;
 }
 
 .new-listing-page label {
@@ -289,23 +369,41 @@ export default {
 }
 
 .new-listing-page input[type="text"],
+.new-listing-page select,
 .new-listing-page input[type="number"] {
-  width: 300px;
   padding: 10px;
-  border: 1px solid #ccc; /* Adjust border color as needed */
+  border: 1px;
   border-radius: 5px;
+  box-sizing: border-box;
+  background-color: #fff;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+input[type="number"] {
+  -moz-appearance: textfield; /* Firefox */
 }
 
 .new-listing-page button {
-  background-color: #007bff; /* Adjust button color as needed */
+  background-color: #eb5440; /* Adjust button color as needed */
   color: #fff; /* Adjust button text color as needed */
   border: none;
   padding: 10px 20px;
   cursor: pointer;
   border-radius: 5px;
+  margin-left: 250px;
+  width: 170px;
+  font-size: 18px;
 }
-
-.new-listing-page button:hover {
-  background-color: #0056b3; /* Adjust hover color as needed */
+.button-active {
+  cursor: pointer;
+  background-color: #eb5440;
+  color: #fff;
+}
+.new-listing-page button:disabled {
+  background-color: #cccccc; /* A grayed out color indicating it is disabled */
+  cursor: not-allowed;
 }
 </style>
